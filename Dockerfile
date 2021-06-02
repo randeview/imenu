5dde888ca9d4bb985e7c2dacdd6e8dc435d9ff3b
randeview <nurlantukenbayev@gmail.com>
2021-06-03 03:52:12 +0600

    Sa task 2

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,7 @@
 FROM python:3.7-slim-buster
-WORKDIR /app
-COPY . /app
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
-
-COPY ./docker-entrypoint.sh /docker-entrypoint.sh
-RUN chmod +x /docker-entrypoint.sh
-ENV DJANGO_SETTINGS_MODULE=config.settings
-ENV PORT 8000
-ENV STATIC_ROOT /static
-ENV MEDIA_ROOT /media
-RUN python manage.py collectstatic --noinput
-
-CMD ["/docker-entrypoint.sh"]
+WORKDIR /app
+COPY . /app
+EXPOSE 80
+CMD ["python", "manage.py", "runserver", "0.0.0.0:80"]
--- config/settings.py
+++ config/settings.py
@@ -84,8 +84,14 @@ AUTH_USER_MODEL = "users.MainUser"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
